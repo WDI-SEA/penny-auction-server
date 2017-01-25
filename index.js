904d2e8ff3504226504b66f25a31462e0b977c5f
Steve Geluso <stevegeluso@gmail.com>
2017-01-25 15:55:46 -0800

    adding CORS middleware

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 var fake_data = require('./fake_data.json');
 var express = require('express');
 var app = express();
+//
+// var BASE_AUCTIONS = {
+//   [
+//     {
+//       title:
+//       img:
+//     }
+//   ]
+// }
+//
+// var AUCTIONS = {
+//   [
+//     {
+//       title:'',
+//       img:','
+//       time_left:
+//       price:
+//       last_bidder:
+//     }
+//   ]
+// }
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 app.get('/poll_item/:id', function(req, res) {
   // haha, ignore the id and just return whatever.
