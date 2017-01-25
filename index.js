255ab9900b767d2040e26534e7bc917b53789aa9
Steve Geluso <stevegeluso@gmail.com>
2017-01-25 15:46:20 -0800

    creating fake data server

---
+++ index.js
@@ -0,0 +1,18 @@
+var fake_data = require('./fake_data.json');
+var express = require('express');
+var app = express();
+
+app.get('/poll_item/:id', function(req, res) {
+  // haha, ignore the id and just return whatever.
+  var index = Math.floor(Math.random() * fake_data.length);
+  var choice = fake_data[index];
+  res.send(choice);
+});
+
+app.get('/*', function(req, res) {
+  res.send({
+    error: "Invalid API endpoint."
+  });
+});
+
+app.listen(3001);
